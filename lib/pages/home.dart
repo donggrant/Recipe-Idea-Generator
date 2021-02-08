36b9cbb61938988510c66a953ccd37e60e62ec1c
Adeet Patel <adeet.patel@gmail.com>
2021-02-07 22:39:20 -0500

    Stream provider query snapshot

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:recipic/models/constants.dart';
 import 'package:recipic/services/auth.dart';
+import 'package:recipic/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Home extends StatelessWidget {
 
@@ -8,23 +11,26 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          elevation: 0.0,
-          title: Text('Home Page'),
-          actions: [
-            FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('Sign Out'),
-              onPressed: () async {
-                await _auth.signOut();
-                Constants().setPageToShow("Sign In");
-              },
-            )
-          ],
-        ),
-        body: Container()
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().userData,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            elevation: 0.0,
+            title: Text('Home Page'),
+            actions: [
+              FlatButton.icon(
+                icon: Icon(Icons.person),
+                label: Text('Sign Out'),
+                onPressed: () async {
+                  await _auth.signOut();
+                  Constants().setPageToShow("Sign In");
+                },
+              )
+            ],
+          ),
+          body: Container(),
+      ),
     );
   }
 }
