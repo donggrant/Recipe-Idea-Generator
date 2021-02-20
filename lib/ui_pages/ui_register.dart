d0b0bf208aa0b863113cda288f55f77cedaec885
srujanjoshi <srujanjoshi7@gmail.com>
2021-02-21 01:07:13 +0530

    Fixed the bottom-overflow pixel warning of the RegisterUI page

--- lib/ui_pages/ui_register.dart
+++ lib/ui_pages/ui_register.dart
@@ -49,146 +49,148 @@ class _RegisterUIState extends State<RegisterUI> {
     return Scaffold(
       backgroundColor: Color(0xFF6C63FF),
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Ink(
-                  //Can add background color to back button here
-                  decoration: ShapeDecoration(
-                    shape: CircleBorder(),
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Ink(
+                    //Can add background color to back button here
+                    decoration: ShapeDecoration(
+                      shape: CircleBorder(),
+                    ),
+                    child: BackButton(
+                      onPressed: () {
+                        Constants().setPageToShow("Landing");
+                      },
+                    ),
                   ),
-                  child: BackButton(
-                    onPressed: () {
-                      Constants().setPageToShow("Landing");
+                  Text(
+                    "Create \nAccount",
+                    style: GoogleFonts.lato(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextFormField(
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      labelText: "Your Email",
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      hintText: 'Email',
+                      hintStyle: TextStyle(color: Colors.grey[400]),
+                    ),
+                    validator: (val) {
+                      if (val.isEmpty)
+                        return 'Enter an email';
+                      else
+                        return null;
+                    },
+                    onChanged: (val) {
+                      setState(() => email = val);
                     },
                   ),
-                ),
-                Text(
-                  "Create \nAccount",
-                  style: GoogleFonts.lato(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                TextFormField(
-                  style: TextStyle(color: Colors.black),
-                  decoration: InputDecoration(
-                    labelText: "Your Email",
-                    fillColor: Colors.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    style: TextStyle(color: Colors.black),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      hintText: 'password',
+                      hintStyle: TextStyle(color: Colors.grey[400]),
                     ),
-                    hintText: 'Email',
-                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    validator: (val) {
+                      if (val.length < 6)
+                        return 'Enter a password 6+ chars long';
+                      else
+                        return null;
+                    },
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    },
                   ),
-                  validator: (val) {
-                    if (val.isEmpty)
-                      return 'Enter an email';
-                    else
-                      return null;
-                  },
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  style: TextStyle(color: Colors.black),
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Password",
-                    fillColor: Colors.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                  SizedBox(height: 15),
+                  TextFormField(
+                    style: TextStyle(color: Colors.black),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: "Confirm Password",
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      hintText: 'Confirm Password',
+                      hintStyle: TextStyle(color: Colors.grey[400]),
                     ),
-                    hintText: 'password',
-                    hintStyle: TextStyle(color: Colors.grey[400]),
+                    validator: (val) {
+                      if (val != password)
+                        return 'Passwords do not match';
+                      else
+                        return null;
+                    },
                   ),
-                  validator: (val) {
-                    if (val.length < 6)
-                      return 'Enter a password 6+ chars long';
-                    else
-                      return null;
-                  },
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(height: 15),
-                TextFormField(
-                  style: TextStyle(color: Colors.black),
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Confirm Password",
-                    fillColor: Colors.white,
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    hintText: 'Confirm Password',
-                    hintStyle: TextStyle(color: Colors.grey[400]),
-                  ),
-                  validator: (val) {
-                    if (val != password)
-                      return 'Passwords do not match';
-                    else
-                      return null;
-                  },
-                ),
-                SizedBox(height: 20),
-                //TODO: Add an agree to terms and conditions and privacy policy
-                GestureDetector(
-                  onTap: () async {
-                    if (_formKey.currentState.validate()) {
-                      dynamic result = await _auth.registerWithEmailAndPassword(
-                          email, password);
-                      if (result == null) {
-                        setState(() {
-                          error = 'please supply a valid email';
-                        });
-                      } else {
-                        Constants().setPageToShow("Sign In");
-                        emailVerificationDialog();
+                  SizedBox(height: 20),
+                  //TODO: Add an agree to terms and conditions and privacy policy
+                  GestureDetector(
+                    onTap: () async {
+                      if (_formKey.currentState.validate()) {
+                        dynamic result = await _auth
+                            .registerWithEmailAndPassword(email, password);
+                        if (result == null) {
+                          setState(() {
+                            error = 'please supply a valid email';
+                          });
+                        } else {
+                          Constants().setPageToShow("Sign In");
+                          emailVerificationDialog();
+                        }
                       }
-                    }
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(18.0),
-                      child: Center(
-                        child: Text(
-                          "Create Account",
-                          style: GoogleFonts.lato(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              textStyle: TextStyle(color: Colors.black)),
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(18.0),
+                        child: Center(
+                          child: Text(
+                            "Create Account",
+                            style: GoogleFonts.lato(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                textStyle: TextStyle(color: Colors.black)),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(height: 12),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 14),
-                ),
-                // SignInButton(
-                //   Buttons.Google,
-                //   text: "Sign up with Google",
-                //   onPressed: () {
-                //     //TODO: Add Google Login
-                //   },
-                // ),
-              ],
+                  SizedBox(height: 12),
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize: 14),
+                  ),
+                  // SignInButton(
+                  //   Buttons.Google,
+                  //   text: "Sign up with Google",
+                  //   onPressed: () {
+                  //     //TODO: Add Google Login
+                  //   },
+                  // ),
+                ],
+              ),
             ),
           ),
         ),
