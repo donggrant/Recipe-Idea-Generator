59e2f0228a156104adc4155e9f086abcc755a76f
srujanjoshi <srujanjoshi7@gmail.com>
2021-02-21 01:01:21 +0530

    Integrated Register Functionality into RegisterUI

--- lib/pages/wrapper.dart
+++ lib/pages/wrapper.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:recipic/models/constants.dart';
 import 'package:recipic/pages/forgot_password.dart';
 import 'package:recipic/pages/home.dart';
-import 'package:recipic/pages/register.dart';
 import 'package:recipic/pages/sign_in.dart';
 import 'package:recipic/ui_pages/ui_landing.dart';
+import 'package:recipic/ui_pages/ui_register.dart';
 
 class Wrapper extends StatefulWidget {
   @override
@@ -26,7 +26,7 @@ class _WrapperState extends State<Wrapper> {
             case "Sign In":
               return SignIn();
             case "Register":
-              return Register();
+              return RegisterUI();
             case "Forgot Password":
               return ForgotPassword();
             case "Home":
--- lib/ui_pages/ui_register.dart
+++ lib/ui_pages/ui_register.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:recipic/models/constants.dart';
+import 'package:recipic/services/auth.dart';
 
 class RegisterUI extends StatefulWidget {
   @override
@@ -10,7 +11,38 @@ class RegisterUI extends StatefulWidget {
 }
 
 class _RegisterUIState extends State<RegisterUI> {
-  List<bool> isSelected = [false, false];
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  // text field state
+  String email = '';
+  String password = '';
+  String error = '';
+
+  void emailVerificationDialog() {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Verification Email Sent'),
+          content: SingleChildScrollView(
+            child: Text("We have sent you an email containing a link to "
+                "verify your email address. You must verify your email "
+                "address before you can sign in."),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,76 +51,145 @@ class _RegisterUIState extends State<RegisterUI> {
       body: SafeArea(
         child: Container(
           margin: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Ink(
-                //Can add background color to back button here
-                decoration: ShapeDecoration(
-                  shape: CircleBorder(),
-                ),
-                child: BackButton(),
-              ),
-              Text(
-                "Create \nAccount",
-                style: GoogleFonts.lato(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                style: TextStyle(color: Colors.black),
-                decoration: InputDecoration(
-                  labelText: "Your Email",
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Ink(
+                  //Can add background color to back button here
+                  decoration: ShapeDecoration(
+                    shape: CircleBorder(),
                   ),
-                  hintText: 'johndoe@mail.com',
-                  hintStyle: TextStyle(color: Colors.grey[400]),
-                ),
-              ),
-              TextFormField(
-                style: TextStyle(color: Colors.black),
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
+                  child: BackButton(
+                    onPressed: () {
+                      Constants().setPageToShow("Landing");
+                    },
                   ),
-                  hintText: 'password',
-                  hintStyle: TextStyle(color: Colors.grey[400]),
                 ),
-              ),
-              //TODO: Add an agree to terms and conditions and privacy policy
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
-                child: Padding(
-                  padding: const EdgeInsets.all(18.0),
-                  child: Center(
-                    child: Text(
-                      "Create Account",
-                      style: GoogleFonts.lato(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          textStyle: TextStyle(color: Colors.black)),
+                Text(
+                  "Create \nAccount",
+                  style: GoogleFonts.lato(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextFormField(
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
+                    labelText: "Your Email",
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'Email',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                  ),
+                  validator: (val) {
+                    if (val.isEmpty)
+                      return 'Enter an email';
+                    else
+                      return null;
+                  },
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  style: TextStyle(color: Colors.black),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'password',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                  ),
+                  validator: (val) {
+                    if (val.length < 6)
+                      return 'Enter a password 6+ chars long';
+                    else
+                      return null;
+                  },
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  },
+                ),
+                SizedBox(height: 15),
+                TextFormField(
+                  style: TextStyle(color: Colors.black),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Confirm Password",
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: 'Confirm Password',
+                    hintStyle: TextStyle(color: Colors.grey[400]),
+                  ),
+                  validator: (val) {
+                    if (val != password)
+                      return 'Passwords do not match';
+                    else
+                      return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                //TODO: Add an agree to terms and conditions and privacy policy
+                GestureDetector(
+                  onTap: () async {
+                    if (_formKey.currentState.validate()) {
+                      dynamic result = await _auth.registerWithEmailAndPassword(
+                          email, password);
+                      if (result == null) {
+                        setState(() {
+                          error = 'please supply a valid email';
+                        });
+                      } else {
+                        Constants().setPageToShow("Sign In");
+                        emailVerificationDialog();
+                      }
+                    }
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Center(
+                        child: Text(
+                          "Create Account",
+                          style: GoogleFonts.lato(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              textStyle: TextStyle(color: Colors.black)),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SignInButton(
-                Buttons.Google,
-                text: "Sign up with Google",
-                onPressed: () {
-                  //TODO: Add Google Login
-                },
-              ),
-            ],
+                SizedBox(height: 12),
+                Text(
+                  error,
+                  style: TextStyle(color: Colors.red, fontSize: 14),
+                ),
+                // SignInButton(
+                //   Buttons.Google,
+                //   text: "Sign up with Google",
+                //   onPressed: () {
+                //     //TODO: Add Google Login
+                //   },
+                // ),
+              ],
+            ),
           ),
         ),
       ),
